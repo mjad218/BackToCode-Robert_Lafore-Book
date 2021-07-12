f270648cb9b04a519b03887cbb1f95c6e5e99e0f
Mohamed Gad <mohamedgad149@gmail.com>
2021-07-12 17:20:38 +0200

    Yeah i love c++

---
+++ Exercises/CH2/Problem 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+/*
+Problem 4 
+Write a program that displays your favorite poem. Use an appropriate escape sequence
+for the line breaks. If you don’t have a favorite poem, you can borrow this one by Ogden
+Nash:
+Candy is dandy,
+But liquor is quicker.*/ 
+
+int main() {
+
+
+	std::cout << "Candy is dandy, \nBut liquor is quicker."; 
+
+	return 0; 
+}
---
+++ Exercises/CH2/Problem 6.cpp
@@ -0,0 +1,44 @@
+﻿/*
+
+Problem 6 
+
+On a certain day the British pound was equivalent to $1.487 U.S., the French franc was
+$0.172, the German deutschemark was $0.584, and the Japanese yen was $0.00955.
+Write a program that allows the user to enter an amount in dollars, and then displays this
+value converted to these four other monetary units.
+
+*/
+ 
+#include <iostream> 
+
+using namespace std; 
+
+/* 
+1 british pound			=>		1.487 dollars 
+
+---						=>		amountOfmoney dollars 
+
+
+*/
+int main() {
+
+	double amountOfmoney; 
+
+
+	cout << "Enter the amount of money you want to convert : "; 
+
+	cin >> amountOfmoney; 
+
+	if (amountOfmoney > 0) {
+		cout << "In British Pound : " << amountOfmoney * 1.487 << endl
+			<< "In French franc : " << amountOfmoney * 0.172 << endl
+			<< "In German deutschemark : " << amountOfmoney * 0.584 << endl;
+
+	} 
+	else {
+	
+		cout << "Money can't be negative" ;
+	}
+
+	return 0; 
+}
