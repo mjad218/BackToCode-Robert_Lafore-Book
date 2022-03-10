629c0243e6d0be68e5c43240061a3b9fbec8e931
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-10 18:13:46 +0200

    pure virtual functions and abstract classes

---
+++ Exercises/CH 11/pure-virtual-functions.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+
+class Animal {
+
+public:
+	//void makeSound() {
+	//	std::cout << "Animal Sound";
+	//}
+	virtual void makeSound() {
+		std::cout << "Animal Sound";
+	}
+};
+
+class Dog : public Animal {
+public:
+	void makeSound() {
+		std::cout << "A Dog Sound" << std::endl;
+	}
+
+};
+class Cat : public Animal {
+public:
+	void makeSound() {
+		std::cout << "A Cat Sound" << std::endl;
+	}
+
+};
+
+int main() {
+	Animal animals; // not alowed Animal is an abstract class
+	Animal* animal;
+	Dog dog;
+	Cat cat;
+	animal = &dog;
+	animal->makeSound();
+	animal = &cat;
+	animal->makeSound();
+
+	return 0;
+}
